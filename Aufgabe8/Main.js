8c398f3a2f20d50732dc0c926e451d66f2747ba4
PedjaGrba <32642575+PedjaGrba@users.noreply.github.com>
2017-12-06 16:39:50 +0100

    Add files via upload

---
+++ Aufgabe8/Main.js
@@ -0,0 +1,32 @@
+/*
+Aufgabe: 8, Manipulation
+Name: Pedja Grba
+Matrikel: 256212
+Datum: 06.12.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+var aufgabe8;
+(function (aufgabe8) {
+    window.addEventListener("load", init);
+    //Große Funktion mit umwandeln der Datentypen.String zu Number + if-Abfragen
+    function init() {
+        let anzahl = prompt("Eingabe der gewünschte Anzahl");
+        let anzahlNumb = parseInt(anzahl);
+        if (anzahlNumb >= 10 && anzahlNumb <= 100) {
+            for (let i = 0; i < anzahlNumb; i++) {
+                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)");
+            }
+        }
+    }
+    //Funktion für Rechteckzeichnung + Parameter für Rechtecke
+    function drawRect(_x, _y, _color) {
+        let div = document.createElement("div"); //erstellt HTML-Element div
+        console.log(div);
+        div.style.marginLeft = _x.toString();
+        div.style.marginTop = _y.toString();
+        div.style.background = _color;
+        document.body.appendChild(div);
+    }
+})(aufgabe8 || (aufgabe8 = {}));
+//# sourceMappingURL=Main.js.map
---
+++ Aufgabe8/Main.ts
@@ -0,0 +1,37 @@
+/*
+Aufgabe: 8, Manipulation
+Name: Pedja Grba
+Matrikel: 256212
+Datum: 06.12.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+namespace aufgabe8 {
+    window.addEventListener("load", init);
+ 
+//Große Funktion mit umwandeln der Datentypen.String zu Number + if-Abfragen
+    function init(): void {
+        let anzahl: string = prompt("Eingabe der gewünschte Anzahl");
+        let anzahlNumb: number = parseInt(anzahl);
+            
+        if (anzahlNumb >= 10 && anzahlNumb <= 100) {
+            for (let i: number = 0; i < anzahlNumb; i++) {
+                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)");
+       } 
+    }
+ }        
+
+ //Funktion für Rechteckzeichnung + Parameter für Rechtecke
+    function drawRect(_x: number, _y: number, _color: string): void {    
+            let div: HTMLDivElement = document.createElement("div"); //erstellt HTML-Element div
+            console.log(div);
+    
+            div.style.marginLeft = _x.toString();
+            div.style.marginTop = _y.toString();
+            div.style.background = _color;
+            
+            document.body.appendChild(div);
+            
+        }   
+}
