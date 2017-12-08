d0e1bda7d4264035d2f2dab1ceea2cf4056b0687
PedjaGrba <32642575+PedjaGrba@users.noreply.github.com>
2017-12-08 09:56:49 +0100

    Add files via upload

--- Aufgabe8/Main.js
+++ Aufgabe8/Main.js
@@ -15,16 +15,18 @@ var aufgabe8;
         let anzahlNumb = parseInt(anzahl);
         if (anzahlNumb >= 10 && anzahlNumb <= 100) {
             for (let i = 0; i < anzahlNumb; i++) {
-                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)");
+                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)", 30, 30);
             }
         }
     }
     //Funktion für Rechteckzeichnung + Parameter für Rechtecke
-    function drawRect(_x, _y, _color) {
+    function drawRect(_x, _y, _color, width, height) {
         let div = document.createElement("div"); //erstellt HTML-Element div
         console.log(div);
-        div.style.marginLeft = _x.toString();
-        div.style.marginTop = _y.toString();
+        div.style.marginLeft = _x + "px";
+        div.style.marginTop = _y + "px";
+        div.style.width = width + "px";
+        div.style.height = height + "px";
         div.style.background = _color;
         document.body.appendChild(div);
     }
--- Aufgabe8/Main.ts
+++ Aufgabe8/Main.ts
@@ -17,18 +17,20 @@ namespace aufgabe8 {
             
         if (anzahlNumb >= 10 && anzahlNumb <= 100) {
             for (let i: number = 0; i < anzahlNumb; i++) {
-                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)");
+                drawRect(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)", 30, 30 );
        } 
     }
  }        
 
  //Funktion für Rechteckzeichnung + Parameter für Rechtecke
-    function drawRect(_x: number, _y: number, _color: string): void {    
+    function drawRect(_x: number, _y: number, _color: string, width: number, height: number): void {    
             let div: HTMLDivElement = document.createElement("div"); //erstellt HTML-Element div
             console.log(div);
     
-            div.style.marginLeft = _x.toString();
-            div.style.marginTop = _y.toString();
+            div.style.marginLeft = _x + "px";
+            div.style.marginTop = _y + "px";
+            div.style.width = width + "px";
+            div.style.height = height + "px";
             div.style.background = _color;
             
             document.body.appendChild(div);
