caa87a2944f47d022dbcb86a9f6cba12bae2c121
PedjaGrba <32642575+PedjaGrba@users.noreply.github.com>
2018-01-16 18:24:30 +0100

    Add files via upload

---
+++ Aufgabe10/data.js
@@ -0,0 +1,33 @@
+/* Name:Predrag Grba
+     Matrikel:256212
+     Datum:07.01.18
+     Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und nicht diktiert.*/
+var Aufgabe10;
+(function (Aufgabe10) {
+    Aufgabe10.baumdaten = [
+        { element: "baum", name: "", preis: 0 },
+        { element: "baum", name: "Fichte", preis: 19 },
+        { element: "baum", name: "Nordmanntanne", preis: 30 }
+    ];
+    Aufgabe10.halterungdaten = [
+        { element: "halterung", name: "", preis: 0 },
+        { element: "halterung", name: "Sternhalterung", preis: 10 },
+        { element: "halterung", name: "Viereckhalterung", preis: 7 }
+    ];
+    Aufgabe10.kugeldaten = [
+        { element: "kugelR", name: "Rot", preis: 10 },
+        { element: "kugelG", name: "Gold", preis: 7 },
+        { element: "kugelB", name: "Blau", preis: 7 }
+    ];
+    Aufgabe10.kerzendaten = [
+        { element: "kerzeR", name: "Rot", preis: 10 },
+        { element: "kerzeB", name: "Bunt", preis: 7 },
+        { element: "kerzeG", name: "Gelb", preis: 7 }
+    ];
+    Aufgabe10.lieferoptionendaten = [
+        { element: "lieferant", name: "DHL", preis: 0 },
+        { element: "lieferant", name: "UPS", preis: 5 },
+        { element: "lieferant", name: "Hermes", preis: 7 }
+    ];
+})(Aufgabe10 || (Aufgabe10 = {}));
+//# sourceMappingURL=data.js.map
