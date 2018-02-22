31d661338e32c9d04204d7d1a344dffcd9c93c93
PedjaGrba <32642575+PedjaGrba@users.noreply.github.com>
2018-02-22 20:40:17 +0100

    Add files via upload

---
+++ Abschlussaufgabe/MovingStadium.js
@@ -0,0 +1,29 @@
+/*
+   Aufgabe: Abschlussaufgabe
+   Name: Pedja Grba
+   Matrikel: 256212
+   Datum: 12.02.2018
+       
+   Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+var abschluss;
+(function (abschluss) {
+    class MovingStadium {
+        constructor(_x, _y) {
+            this.x = _x;
+            this.y = _y;
+        }
+        update() {
+            this.move();
+            this.draw();
+        }
+        move() {
+            //Hallo
+        }
+        draw() {
+            //Hallo
+        }
+    }
+    abschluss.MovingStadium = MovingStadium;
+})(abschluss || (abschluss = {}));
+//# sourceMappingURL=MovingStadium.js.map
---
+++ Abschlussaufgabe/MovingStadium.ts
@@ -0,0 +1,38 @@
+/*
+   Aufgabe: Abschlussaufgabe
+   Name: Pedja Grba
+   Matrikel: 256212
+   Datum: 12.02.2018
+       
+   Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+namespace abschluss {
+
+    export class MovingStadium {
+        x: number;
+        y: number;
+        color: string;
+
+        constructor(_x: number, _y: number) {
+            
+            this.x = _x;
+            this.y = _y;
+        }
+
+
+
+        update(): void {
+            this.move();
+            this.draw();
+        }
+
+        move(): void {
+            //Hallo
+}
+
+        draw(): void {
+            //Hallo
+        }
+    }
+}
